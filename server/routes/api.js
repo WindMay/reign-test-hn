5613c7b3652f6190488a2c01d7917a45aabf61ca
Robert Steelheart <rob.steelheart@gmail.com>
2018-01-21 20:53:27 -0300

    add endpoint to external api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// declare axios for making http requests
+const axios = require('axios');
+const API = 'https://hn.algolia.com/api/v1';
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+// Get latest nodejs related articles
+router.get('/articles', (req, res) => {
+  axios.get(`${API}/search_by_date?query=nodejs`).then(articles => {
+    res.status(200).json(articles.data);
+  }).catch(error => {
+    res.status(500).send(error);
+  });
 });
 
 module.exports = router;
